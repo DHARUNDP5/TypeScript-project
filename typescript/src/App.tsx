beaea7f25530057154cf83bcdd6030ff870b2991
DHARUNDP5 <dharunprakashkdp@gmail.com>
2023-03-16 12:49:31 +0530

    Quize Page Working

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -1,24 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+//components
+import QuestionCard from './Components/QuestionCard';
 
-function App() {
+const TOTAL_OUESTIONS = 10;
+
+const  App=()=> {
+  const [loading,setLoading]=useState(false);
+  const [questions,setQuestons]=useState([]);
+  const [number,setNumber]=useState(0);
+  const [userAnswers,setUserAnswers]=useState([]);
+  const [score,getScore]=useState(0);
+  const [gameOver,setGameOver]=useState(true);
+
+
+  const startTrivia = async ()=>{
+
+  }
+  const checkAnswer = (e:React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  const nextQuestion = async ()=>{
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TYPESCRIPT OUIZ</h1>
+      <button className='start' onClick={startTrivia}>Start</button>
+    <p className='score'>Score</p>
+    <p>Loading Questions...</p>
+    <QuestionCard
+    questionNr={number+1}
+    totalq={TOTAL_OUESTIONS}
+    questions={questions[number].question}
+    answers={questions[number].answers}
+    userAnswer={userAnswers?userAnswers[number]}
+    />
+    <button className='next' onClick={nextQuestion}>
+      Next
+    </button>
     </div>
   );
 }
---
+++ typescript/src/Components/QuestionCard.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+type Props ={
+    question:string;
+    answers:string[];
+    callback:any;
+    userAnswer:boolean;
+    questionNr:number;
+    totalq:number;
+}
+
+const QuestionCard:React.FC<Props> =({
+    question,
+    answers,
+    callback,
+    userAnswer,
+    questionNr,
+    totalq,
+}) => (
+  
+    <div>
+        <p className='number'>
+            Questions:{questionNr}/{totalq}
+        </p>
+        <p dangerouslySetInnerHTML={{ __html:question}}/>
+        <div>
+            {answers.map((answer) => (
+                <div>
+                    <button disabled={userAnswer} onClick={callback}>
+                        <span dangerouslySetInnerHTML={{ __html:answer }}/>
+                    </button>
+                   <div/>
+          )) }
+        </div>
+    </div>
+);
+  
+
+export default QuestionCard;
